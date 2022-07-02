14ea068ae1a5f6a881b81d77bfbf0b98af3a39c0
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2022-07-02 14:32:23 +0100

    added the popup

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -3,17 +3,19 @@ import './Popup.css'
 
 const Popup = (props) => {
 
+    if (!props.trigger) return null;
+
     const handleClick = () => {
         props.setTrigger(false);
     }
-    return (props.trigger) ? (
+    return (
         <div className="popup">
             <div className="popup-inner">
                 <button className="close-btn" onClick={handleClick}>close</button>
                 {props.children}
             </div>
         </div>
-    ) : "";
+    )
 }
 
 export default Popup
