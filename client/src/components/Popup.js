f7ad518f1c3d06170075c4b3c31098aff67741a9
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2022-07-05 15:33:34 +0100

    forms are styled

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -11,7 +11,7 @@ const Popup = (props) => {
     return (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn" onClick={handleClick}>close</button>
+                <span className="close close-btn" onClick={handleClick}>&times;</span>
                 {props.children}
             </div>
         </div>
